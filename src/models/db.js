d1538b590eca4d9fa539efc286bb11a5feb9c742
saintworld09 <ababatunde306@gmail.com>
2026-05-12 12:29:40 +0100

    updated week 2 learning

--- src/models/db.js
+++ src/models/db.js
@@ -12,8 +12,10 @@ import { Pool } from 'pg';
  * postgresql://username:password@host:port/database
  */
 const pool = new Pool({
-    connectionString: process.env.DB_URL,
-    ssl: true
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+        rejectUnauthorized: false
+    }
 });
 
 /**
