d1eb2ea5112e9d5720270d35fd882f68e559a078
saintworld09 <ababatunde306@gmail.com>
2026-05-13 08:58:57 +0100

    pool connection error fixed

--- src/models/db.js
+++ src/models/db.js
@@ -13,9 +13,9 @@ import { Pool } from 'pg';
  */
 const pool = new Pool({
     connectionString: process.env.DATABASE_URL,
-    ssl: process.env.NODE_ENV === 'production'
-        ? { rejectUnauthorized: false }
-        : false
+    ssl: {
+        rejectUnauthorized: false
+    }
 });
 
 /**
