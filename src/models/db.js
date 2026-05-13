8e6158aa2f31bfaafa77c2108968fe2cfe457ad0
saintworld09 <ababatunde306@gmail.com>
2026-05-13 08:45:34 +0100

    Fixed SSL configuration for development and production

--- src/models/db.js
+++ src/models/db.js
@@ -13,9 +13,9 @@ import { Pool } from 'pg';
  */
 const pool = new Pool({
     connectionString: process.env.DATABASE_URL,
-    ssl: {
-        rejectUnauthorized: false
-    }
+    ssl: process.env.NODE_ENV === 'production'
+        ? { rejectUnauthorized: false }
+        : false
 });
 
 /**
